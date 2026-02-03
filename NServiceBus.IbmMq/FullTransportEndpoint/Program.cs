bffd942ca606f7b2ef5c67a40a7cb9cbce8264a8
Ramon Smits <ramon.smits@gmail.com>
2026-02-03 12:04:43 +0100

    Refactored FullTransportEndpoint and fixed encoding issues in IBM MQ transport

--- NServiceBus.IbmMq/FullTransportEndpoint/Program.cs
+++ NServiceBus.IbmMq/FullTransportEndpoint/Program.cs
@@ -1,27 +1,45 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NServiceBus.IbmMq;
 
 Console.Title = "Bridge";
 var builder = Host.CreateApplicationBuilder(args);
 
-var ibmmq = new IbmMqTransport("localhost", "admin", "passw0rd", null, null));
+var ibmmq = new IbmMqTransport("localhost", "admin", "passw0rd", null, null);
 
 var endpointB = new EndpointConfiguration("DEV.SHIPPING");
+endpointB.SendFailedMessagesTo("error");
+endpointB.UseTransport(ibmmq);
+endpointB.UseSerialization<SystemJsonSerializer>();
+endpointB.PurgeOnStartup(true);
 
-//endpointB.RegisterPublisher<OrderPlaced>("DEV.SALES");
-
-//ibmmq.HasEndpoint(endpointB);
-
-//var endpointA = new BridgeEndpoint("DEV.SALES");
-
-//endpointA.RegisterPublisher<OrderShipped>("DEV.SHIPPING");
-
-//learning.HasEndpoint(endpointA);
-
-//bridgeConfiguration.AddTransport(ibmmq);
-//bridgeConfiguration.AddTransport(learning);
-//builder.UseNServiceBusBridge(bridgeConfiguration);
+builder.UseNServiceBus(endpointB);
 
 var host = builder.Build();
 
-await host.RunAsync();
+await host.StartAsync();
+
+var instance = host.Services.GetRequiredService<IMessageSession>();
+
+while (true)
+{
+    Console.ReadLine();
+    Console.WriteLine("Sending message...");
+    await instance.SendLocal(new MyMessage());
+}
+
+
+await host.StopAsync();
+
+
+class MyHandler : IHandleMessages<MyMessage>
+{
+    public Task Handle(MyMessage message, IMessageHandlerContext context)
+    {
+        Console.WriteLine("Received message!");
+        return Task.CompletedTask;
+    }
+}
+
+
+record MyMessage(string Data = "Hello World!");
