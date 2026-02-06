07af800aa827cbecd250936366f1613315c37f20
Ramon Smits <ramon.smits@gmail.com>
2026-02-06 12:45:58 +0100

    Reuse topic and queue objects in dispatcher

--- src/NServiceBus.IbmMq/FullTransportEndpoint.Subscriber/MyHandler.cs
+++ src/NServiceBus.IbmMq/FullTransportEndpoint.Subscriber/MyHandler.cs
@@ -7,7 +7,16 @@ sealed class MyHandler : IHandleMessages<MyMessage>
     public async Task Handle(MyMessage message, IMessageHandlerContext context)
     {
         Console.WriteLine($"Start {message.Data}");
+        await context.SendLocal(new MyMessage2());
+        await context.SendLocal(new MyMessage2());
         await Task.Delay(200, context.CancellationToken);
         Console.WriteLine($"End: {message.Data}");
+    }
+}
+sealed class MyHandler2 : IHandleMessages<MyMessage2>
+{
+    public async Task Handle(MyMessage2 message, IMessageHandlerContext context)
+    {
+        Console.WriteLine($"MyMessage2");
     }
 }
